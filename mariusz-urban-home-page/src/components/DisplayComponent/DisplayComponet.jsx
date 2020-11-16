2a2f6a894f09bf869593dc1dada4cec0339051fe
Mariusz <urban.mariusz@icloud.com>
2020-11-16 12:01:47 +0100

    setup gotowy

---
+++ mariusz-urban-home-page/src/components/DisplayComponent/DisplayComponet.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './_displayComponent.scss'
+export default class DisplayComponet extends Component {
+    render() {
+        return (
+            <div className='displayComponent'>
+                CHUJ
+            </div>
+        )
+    }
+}
